cad1b15f44627c354c80ca98cc28e7b53f635e1f
Stephanie Conde <stephaniecondecareer@gmail.com>
2022-12-16 16:25:06 -0800

    commit tree 6b1bbebb4e6190f9c7410d3469aa2569e676d336

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,3 +1,18 @@
 class Vehicle
 
+  attr_accessor :wheel_size, :wheel_number
+
+  def initialize(wheel_size, wheel_number)
+    @wheel_size = wheel_size
+    @wheel_number = wheel_number
+  end
+
+  def go
+    "vrrrrrrrooom!"
+  end
+
+  def fill_up_tank
+    "filling up!"
+  end
+  # Instances of Vehicle initialize with a wheel size and number. We also have #go and #fill_up_tank instance methods that describe some common vehicle behavior.
 end
